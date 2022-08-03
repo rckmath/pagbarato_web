6881936ddc0b5331199f9a6227df763f2d6fecf2
Erick M. L. Pacheco <ericklopes02@gmail.com>
2022-08-02 23:50:45 -0300

    feat: removed state initializing from context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,14 +1,9 @@
 import { User, UserCredential } from 'firebase/auth'
 import { createContext } from 'react'
-
-const INITIAL_STATE = {
-  user: JSON.parse(sessionStorage.getItem('user') as string) || null,
-}
-
 export interface IUserContext {
   user: User | null
-  logIn?: (email: string, password: string) => Promise<UserCredential>
-  logOut?: () => Promise<void>
+  logIn: (email: string, password: string) => Promise<UserCredential>
+  logOut: () => Promise<void>
 }
 
-export const UserContext = createContext<IUserContext>(INITIAL_STATE)
+export const UserContext = createContext<IUserContext>(null!)
