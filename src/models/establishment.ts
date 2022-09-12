c7d45993ed925fd71355ffcc17b6dcdc869410f5
Erick M. L. Pacheco <ericklopes02@gmail.com>
2022-09-12 20:41:11 -0300

    feat: main establishment business hours form system created

--- src/models/establishment.ts
+++ src/models/establishment.ts
@@ -1,3 +1,36 @@
+export const DaysOfWeekType: { [x: string]: 'SUN' | 'MON' | 'TUES' | 'WED' | 'THURS' | 'FRI' | 'SAT' | 'HOLIDAYS' } = {
+  SUN: 'SUN',
+  MON: 'MON',
+  TUES: 'TUES',
+  WED: 'WED',
+  THURS: 'THURS',
+  FRI: 'FRI',
+  SAT: 'SAT',
+  HOLIDAYS: 'HOLIDAYS',
+};
+
+export type DaysOfWeekType = typeof DaysOfWeekType[keyof typeof DaysOfWeekType];
+
+export const DaysOfWeekTypeMap = [
+  [DaysOfWeekType.SUN, 'Domingo'],
+  [DaysOfWeekType.MON, 'Segunda'],
+  [DaysOfWeekType.TUES, 'Terça'],
+  [DaysOfWeekType.WED, 'Quarta'],
+  [DaysOfWeekType.THURS, 'Quinta'],
+  [DaysOfWeekType.FRI, 'Sexta'],
+  [DaysOfWeekType.SAT, 'Sábado'],
+  [DaysOfWeekType.HOLIDAYS, 'Feriados'],
+];
+
+export const MAX_BUSINESSES_HOURS = 8;
+
+export type BusinessHours = {
+  id?: string;
+  day: DaysOfWeekType;
+  openingAt: Date | string | null;
+  closureAt: Date | string | null;
+};
+
 export type Establishment = {
   id: string;
   name: string;
@@ -11,6 +44,7 @@ export type EstablishmentForm = {
   name: string;
   latitude: number;
   longitude: number;
+  businessHours: Array<BusinessHours>;
   createdAt: Date | string;
   updatedAt?: Date | string;
 };
